1b0b04815afa86ea8015dcb979fb8bc203a673af
wutermeloon <52638145+wutermeloon@users.noreply.github.com>
2019-10-08 18:23:35 -0700

    Line Stuff
    These programs include the Line_Flower, squaring to the line, and color calibration. So far, the square to line could use a few changes, it doesn't completely work yet

---
+++ light_calib.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env micropython
+
+import sys
+
+from ev3dev2.motor import OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D
+from ev3dev2.motor import LargeMotor, MediumMotor, MoveTank, MoveSteering
+from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4
+from ev3dev2.sensor.lego import ColorSensor
+
+m = MoveSteering(OUTPUT_B, OUTPUT_C)
+cl = ColorSensor(INPUT_1)
+cr = ColorSensor(INPUT_4)
+l_black = 100
+l_white = 0
+r_black = 100
+r_white = 0
+m.left_motor.position = 0
+m.on(0, 20)
+
+while m.left_motor.position < 360:
+    rli = cl.reflected_light_intensity
+    if l_black > rli:
+        l_black = rli
+    if l_white < rli:
+        l_white = rli
+    rli = cr.reflected_light_intensity
+    if r_black > rli:
+        r_black = rli
+    if r_white < rli:
+        r_white = rli
+m.off()
+
+print(l_black, file=sys.stderr)
+print(l_white, file=sys.stderr)
+print(r_black, file=sys.stderr)
+print(r_white, file=sys.stderr)
+
+o=open("data_calib.py", "w")
+o.write("l_black = " + str(l_black) + "\n")
+o.write("l_white = " + str(l_white) + "\n")
+o.write("r_black = " + str(r_black) + "\n")
+o.write("r_white = " + str(r_white) + "\n")
+o.close()
