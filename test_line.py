7ddbcd29df0995b35e211803c25832c93bbd11df
wutermeloon <52638145+wutermeloon@users.noreply.github.com>
2019-10-18 18:48:26 -0700

    New Line_Flowering Parameter
    Some new parameters have been added, stop_at_end and min_speed. Also, some parameters have been given default values so that they don't take up as much space.

--- test_line.py
+++ test_line.py
@@ -9,6 +9,10 @@ os.system('setfont Lat15-TerminusBold32x16')
 from line import Line_Flowering, square2line
 from color import *
 
-#Line_Flowering(right_color_sensor_rli, is_left_white, 2, -50)
+def never():
+    return False
 
-square2line()
+Line_Flowering(right_color_sensor_rli, is_left_white, 2, stop_at_end=True)
+
+#square2line()
+
