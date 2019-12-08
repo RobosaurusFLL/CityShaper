36c0a09e51a638e57ce29629780986a394facf9b
wutermeloon <52638145+wutermeloon@users.noreply.github.com>
2019-12-08 14:17:19 -0800

    Changed mission order
    Instead of going to the crane and then backing onto the circle-ish line to get to the tree, we will go to the tree and then go straight to the crane.

--- Traffic_Crane.py
+++ Traffic_Crane.py
@@ -14,7 +14,7 @@ ml=LargeMotor(OUTPUT_B)
 mr=LargeMotor(OUTPUT_C)
 mmL=MediumMotor(OUTPUT_A)
 
-def traffic_crane_mission():
+def traffic2circle_ish_line():
     #going backwards to bump into traffic mission; following the south wall
     m.on_for_rotations(-5, 50, 2.5)
     m.on_for_seconds(-5, 20, 2.5)
@@ -24,46 +24,15 @@ def traffic_crane_mission():
         pass
     m.off()
     #Turning towards crane mission
-    m.on(33, -40)
+    m.on(30, -40)
     #Stoping at line connected to base
-    while is_left_black() != True:
+    while is_right_black() != True:
         pass
     m.off()
-    m.on(0, -20)
-    while is_left_white() != True:
-        pass
-    #Finding dark blue area outside white circle
-    while left_color_sensor_rli() > 50:
-        pass
-    m.off()
-    # turning so that the right color sensor doesn't instantly sense white   
-    m.on_for_rotations(-50, -40, 0.8)
-    #Following edge of white circle
-    Line_Flowering(left_color_sensor_rli, is_right_white, 1.5)
-    #Pushing attachment into crane
-    m.on_for_seconds(0, -10, 3)
 
-def crane2tree():
-    #turning and then running towards tree
-
-    #backing into begining of circle-ish line
-    m.on(0, 40)
-    while not is_right_white():
-        pass
-    while not is_right_black():
-        pass
-    while not is_right_white():
-        pass
-    while not is_right_other_shade():
-        pass
-    while not is_right_white():
-        pass
-    m.off()
-    m.on(40, 30)
-    while not is_right_black():
-        pass
+def line2tree():
 #following circle-ish line to tree
-    m.on_for_rotations(0, -30, 0.35)
+    m.on_for_rotations(0, -30, 0.15)
     m.on(100, -40)
     while not is_right_black():
         pass
@@ -81,28 +50,71 @@ def crane2tree():
         return mr.position < end_position
 #using difference of motors to see how much the robot has turned on the circle-ish line
     def stoping_point():
-        return mr.position - ml.position < -200
-    Line_Flowering(rlidiff, stoping_point, 1.5, min_speed=-15)
+        #print(mr.position - ml.position, file=sys.stderr)
+        if abs(ml.position) > abs(mr.position):
+            mr.position = 0
+            ml.position = 0
+        return mr.position - ml.position < -700
+    Line_Flowering(rlidiff, stoping_point, 1.5, min_speed=-30)
 
-    end_position = mr.position - (360 * 1.2)
-    Line_Flowering(rlidiff, on_for_rotations, 1.5, stop_at_end=True)
+    end_position = mr.position - (360 * 1.1)
+    Line_Flowering(rlidiff, on_for_rotations, 1, max_speed=-30, stop_at_end=True)
 #activating attachment to push blocks onto tree
     mmL.on_for_seconds(-75, 2)
     time.sleep(1)
-    
-def tree2home():
-    m.on_for_rotations(0, 20, 0.5)
+
+def tree2crane():
+    #m.on_for_rotations(0, 25, 0.1)
+    m.on_for_rotations(-100, -30, 1.3)
+
+    ml.on(20)
+    while not is_left_white():
+        pass
+    ml.off()
+    mr.on(20)
+    while not is_right_white():
+        pass
+    mr.off()
+    ml.on(20)
+    while not is_left_black():
+        pass
+    ml.off()
+    mr.on(20)
+    while not is_right_black():
+        pass
+    mr.off()
+
+    m.on_for_rotations(0, -40, 2.5)
+    m.on(0, -30)
+    while not is_left_white():
+        pass
+    while not is_left_black():
+        pass
+    m.off()
+
+    m.on_for_rotations(100, -30, 1.3)
+    m.on(0, -30)
+    while not is_right_black():
+        pass
+    while not is_right_white():
+        pass
+    m.off()
+    m.on_for_seconds(0, -10, 1.5)
+
+def crane2home():
+    m.on_for_rotations(0, 60, 0.5)
     m.on_for_rotations(0, 100, 1)
     m.on_for_rotations(100, -30, 1.5)
-    m.on_for_seconds(0, 100, 4)
+    m.on_for_seconds(0, 100, 2.5)
     m.on_for_seconds(-100, -50, 1)
     #while not is_right_black():
     #    pass
 
 def Traffic_Crane():
-    traffic_crane_mission()
-    crane2tree()
-    tree2home()
+    traffic2circle_ish_line()
+    line2tree()
+    tree2crane()
+    crane2home()
 
 
 if __name__ == "__main__":
